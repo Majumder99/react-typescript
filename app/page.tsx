2d412591c59966a51f08044ff1c8de478e5cc000
Sourav Majumder <souravmajumderk99@gmail.com>
2023-11-28 15:58:21 +0600

    type

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,24 @@ import Button from "@/components/Button";
 import Image from "next/image";
 
 export default function Home() {
+  const onclick = () => {
+    console.log("I'm clicked");
+  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button bgColor="red" fontSize={23} phillShape={true} />
+      {/* <Button
+        bgColor="red"
+        fontSize={23}
+        phillShape={true}
+        padding={[10, 2, 2]}
+      /> */}
+      {/* <Button
+        style={{
+          backgroundColor: "blue",
+          fontSize: 24,
+        }}
+      /> */}
+      <Button onClick={onclick} />
     </main>
   );
 }
--- components/Button.tsx
+++ components/Button.tsx
@@ -2,20 +2,53 @@ import React from "react";
 
 let url: string = "https://www.google.com";
 
-type ButtonProps = {
-  bgColor: string;
-  fontSize: number;
-  phillShape: boolean;
-};
+// type ButtonProps = {
+//   bgColor: string;
+//   fontSize: number;
+//   phillShape?: boolean; //it can be null or boolean
+//   // padding: number[]
+//   padding: [number, number, number]; //tuple
+// };
 
-const Button = ({ bgColor, fontSize, phillShape }: ButtonProps) => {
+// const Button = ({ bgColor, fontSize, phillShape, padding }: ButtonProps) => {
+//   // const bgColor = props.bgColor;
+//   //   const { bgColor } = props;
+//   return (
+//     <>
+//       <div className="bg-blue-500 text-white rounded px-4 py-2">Click Me</div>
+//     </>
+//   );
+// };
+
+type ButtonProps = {
+  onClick: () => void;
+};
+// type ButtonProps = {
+//   style: React.CSSProperties;
+// };
+// type ButtonProps = {
+//   style: {
+//     backgroundColor: string;
+//     fontSize: number;
+//   };
+// };
+
+const Button = ({ onClick }: ButtonProps) => {
   // const bgColor = props.bgColor;
   //   const { bgColor } = props;
   return (
     <>
-      <div className="bg-blue-500 text-white rounded px-4 py-2">Click Me</div>
+      <div onClick={onClick}>Click Me</div>
     </>
   );
+  // const Button = ({ style }: ButtonProps) => {
+  //   // const bgColor = props.bgColor;
+  //   //   const { bgColor } = props;
+  //   return (
+  //     <>
+  //       <div style={style}>Click Me</div>
+  //     </>
+  //   );
 };
 
 export default Button;
